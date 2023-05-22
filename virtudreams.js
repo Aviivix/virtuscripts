43e9fc19493c11d86003a0780e2c3cc5c2185cff
Aviivix <47121982+Aviivix@users.noreply.github.com>
2023-05-22 12:05:13 -0400

    Update virtudreams.js
    Trying to make it more intuitive to download and update.

--- virtudreams.js
+++ virtudreams.js
@@ -3,6 +3,8 @@
 // @version      0.1.1
 // @description  The VirtuPets Dream Database!
 // @author       Aviivix
+// @downloadURL https://raw.githubusercontent.com/Aviivix/virtuscripts/main/virtudreams.js
+// @updateURL https://raw.githubusercontent.com/Aviivix/virtuscripts/main/virtudreams.js
 // @match        https://virtu.pet/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=virtu.pet
 // @grant GM_xmlhttpRequest
